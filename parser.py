7532014b4cbe41623160e709da4fd5263a266133
Von <vonzedric012@gmail.com>
2025-04-16 16:10:46 +0800

    updated

--- parser.py
+++ parser.py
@@ -3,6 +3,7 @@ def read_str(line):
     idx = 0
 
     while idx + 1 < len(line):
+        print(stack)
         char = line[idx]
         if char == '{':
             empty_li = []
@@ -15,6 +16,34 @@ def read_str(line):
         idx += 1
     return stack[1]
 
+def find_string(line):
+    open_quote = -1
+    for idx, char in enumerate(line):
+        if char == '"' and open_quote != -1:
+            return line[open_quote: idx]
+        if char == '"':
+            open_quote = idx + 1
+    return line 
+
+def read(file_path):
+    file = open(file_path, 'r')
+    lines = file.read().split('\n')
+
+    stack = {{}}
+    idx = 0
+
+    while idx < len(lines):
+        line = lines[idx].lstrip()
+        key = find_string(line)
+        
+            
+
+        
+        line = lines[idx].lstrip()
+        key = find_string(line)
+        args[key] = line[line.find(": "):]
+        idx += 1
+    return args
+
 if __name__ == "__main__":
-    line = "{a{bc{b}}d}"
-    print(read_str(line))
+    print(read("aaa.txt"))   
